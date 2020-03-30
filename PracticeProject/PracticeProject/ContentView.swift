d964b886618cc23b5c7a36fbf8115ce31c7d0b4f
jesseleeruiz <jesseleeruiz@gmail.com>
2020-03-30 09:04:58 -1000

    Added initial project for SwiftUI practice.

---
+++ PracticeProject/PracticeProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PracticeProject
+//
+//  Created by Jesse Ruiz on 3/30/20.
+//  Copyright © 2020 Jesse Ruiz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
