444abab02bd33bdf538a26f73fc5f4ba5f180e6b
jesseleeruiz <jesseleeruiz@gmail.com>
2020-09-30 15:41:47 -1000

    Made some changes to the ContentView in order to understand view states and the view tree.

--- Thinking SwiftUI/Thinking SwiftUI/ContentView.swift
+++ Thinking SwiftUI/Thinking SwiftUI/ContentView.swift
@@ -8,9 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var counter = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Button(
+                action: {counter += 1},
+                label: {
+                Text("Tap me!")
+                    .padding()
+                    .background(Color(.tertiarySystemFill))
+                    .cornerRadius(5)
+            })
+            
+            if counter > 0 {
+                Text("You've tapped \(counter) times")
+            } else {
+                Text("You've not yet tapped")
+            }
+        }
+        .frame(width: 200, height: 200)
+        .border(Color.black)
     }
 }
 
